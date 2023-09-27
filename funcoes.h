fc459ad11ab6cd21f82fdbbad641651888c25e67
theobarreto <theobarreto@usp.br>
2023-09-26 21:44:52 -0300

    Codigo unificado com todas as funcoes

--- funcoes.h
+++
@@ -1,8 +0,0 @@
-#ifndef FUNCOES_H_
-#define FUNCOES_H_
-
-double* cossine(double*array,int tam);
-double* squareroot(double*array,int tam);
-double* square(double*array,int tam);
-
-#endif
--- trabalho1.c
+++ trabalho1.c
@@ -1,7 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-#include "funcoes.h"
+
+double* square(double*array,int tam){ //essa funcao eleva ao quadrado os elementos do array
+    double * squares = (double*)malloc(tam*sizeof(double)); //alocacao de memoria dinamica
+    for (int i=0;i<tam;i++){
+        squares[i]=array[i]*array[i];
+    }
+    return squares;
+} 
+
+double* squareroot(double*array,int tam){ //definicao da funcao raiz
+    double * roots = (double*)malloc(tam*sizeof(double)); //alocacao de memoria dinamica
+    for (int i=0;i<tam;i++){
+        roots[i]=sqrt(array[i]);
+    }
+    return roots;
+}
+
+double* cossine(double*array,int tam){ //definicao da funcao cosseno 
+    double * coss = (double*)malloc(tam*sizeof(double)); 
+    for (int i=0;i<tam;i++){
+        coss[i]=cos(array[i]);
+    }
+    return coss;
+}
+
 
 int main (int argc, char** argv){ //definicao da funcao principal
     int tam;
@@ -23,7 +47,7 @@ int main (int argc, char** argv){ //definicao da funcao principal
         printf("%lf\n", roots[i]);
     }
        double* coss = cossine(array, tam);
-       printf("Esses sao os cossenos dos valores inputados em radianos:\n");
+       printf("Esses sao os cossenos dos valores em radianos:\n");
         for (int i=0;i<tam;i++){
         printf("%lf\n", coss[i]);
     }
